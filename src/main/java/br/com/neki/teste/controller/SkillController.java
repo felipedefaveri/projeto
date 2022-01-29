a1f09fabb2a1cc09778f99a970ddc7e73a1030e1
CasttCK <vini.kronemberger@gmail.com>
2022-01-29 14:15:15 -0300

    Criação endpoints

--- src/main/java/br/com/neki/teste/controller/SkillController.java
+++ src/main/java/br/com/neki/teste/controller/SkillController.java
@@ -3,11 +3,17 @@ package br.com.neki.teste.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.neki.teste.domain.Skill;
@@ -30,6 +36,13 @@ public class SkillController {
     public ResponseEntity<Optional<Skill>> buscarPorId(@PathVariable Integer id) {
         Optional<Skill> skill = skillService.listarPorId(id);
         return ResponseEntity.ok().body(skill);
+    }
+	
+	@PostMapping("/inserir")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Skill inserirHabilidade( @RequestBody @Valid  Skill skill) {
+        return skillService.inserir(skill);
+
 
     }
-} 	
+}
