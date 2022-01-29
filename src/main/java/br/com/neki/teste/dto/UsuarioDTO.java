7aa395d69e2ba6b2a74ceee6ade750d0cdf3c7d1
felipedefaveri <felipefaver27@gmail.com>
2022-01-29 09:08:00 -0300

    criação pacotes service e dto

---
+++ src/main/java/br/com/neki/teste/dto/UsuarioDTO.java
@@ -0,0 +1,24 @@
+package br.com.neki.teste.dto;
+
+public class UsuarioDTO {
+	
+	public String login;
+	public String password;
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public UsuarioDTO() {
+		super();
+	}
+	
+
+}
